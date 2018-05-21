ac9bc2f845bafbf0e1444770cc3f6e6bacf43434
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-05-21 21:53:49 +0200

    notes

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -119,7 +119,7 @@ class Dashboard extends React.Component{
                         <Card className="card-tasks">
                           <CardHeader>
                             <CardTitle>Tasks</CardTitle>
-                            <p className="category">Backend Development</p>
+                            <p className="category">Crypto News</p>
                           </CardHeader>
                           <CardBody>
                             <Tasks tasks={tasks}/>
@@ -137,49 +137,25 @@ class Dashboard extends React.Component{
                       <Col xs={12} md={6}>
                         <Card>
                           <CardHeader>
-                            <CardTitle>Employees Stats</CardTitle>
-                            <p className="category">All Persons List</p>
+                            <CardTitle>Crypto Prices</CardTitle>
+                            <p className="category">From Coinmarketcap</p>
                           </CardHeader>
                           <CardBody>
                             <Table responsive>
                               <thead className=" text-primary">
                                 <tr>
-                                  <th>Name</th>
-                                <th>Country</th>
-                                <th>City</th>
-                                <th className="text-right">Salary</th>
+                                  <th>Currency</th>
+                                <th>Price</th>
+                                <th>Last 24h</th>
+                                <th className="text-right">Market Cap</th>
                                 </tr>
                               </thead>
                               <tbody>
                                 <tr>
-                                  <td>Dakota Rice</td>
-                                  <td>Niger</td>
-                                  <td>Oud-Turnhout</td>
-                                  <td className="text-right">$36,738</td>
-                                </tr>
-                                <tr>
-                                  <td>Minerva Hooper</td>
-                                  <td>Curaçao</td>
-                                  <td>Sinaai-Waas</td>
-                                  <td className="text-right">$23,789</td>
-                                </tr>
-                                <tr>
-                                  <td>Sage Rodriguez</td>
-                                  <td>Netherlands</td>
-                                  <td>Baileux</td>
-                                  <td className="text-right">$56,142</td>
-                                </tr>
-                                <tr>
-                                  <td>Doris Greene</td>
-                                  <td>Malawi</td>
-                                  <td>Feldkirchen in Kärnten</td>
-                                  <td className="text-right">$63,542</td>
-                                </tr>
-                                <tr>
-                                  <td>Mason Porter</td>
-                                  <td>Chile</td>
-                                  <td>Gloucester</td>
-                                  <td className="text-right">$78,615</td>
+                                  <td>todo</td>
+                                  <td>todo</td>
+                                  <td>todo</td>
+                                  <td className="text-right">todo</td>
                                 </tr>
                               </tbody>
                             </Table>
