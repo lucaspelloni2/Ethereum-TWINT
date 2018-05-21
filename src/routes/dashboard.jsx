efc6d669dc5ea7bea331d7583b1c6705011dc69d
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-05-21 20:59:35 +0200

    pro removed

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -15,7 +15,6 @@ var dashRoutes = [
     { path: "/user-page", name: "User Profile", icon: "users_single-02", component: UserPage },
     { path: "/extended-tables", name: "Table List", icon: "files_paper", component: TableList },
     { path: "/typography", name: "Typography", icon: "design-2_ruler-pencil", component: Typography },
-    { pro: true, path: "/upgrade", name: "Upgrade to PRO", icon: "objects_spaceship", component: Upgrade },
     { redirect: true, path: "/", pathTo: "/dashboard", name: "Dashboard" }
 ];
 export default dashRoutes;
