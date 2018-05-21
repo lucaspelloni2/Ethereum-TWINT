332c2f9c701081d5327a8c3c0546726bfb10fbff
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-05-21 21:17:20 +0200

    less component

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -9,12 +9,12 @@ import UserPage from 'views/UserPage/UserPage.jsx';
 
 var dashRoutes = [
     { path: "/dashboard", name: "Portfolio Overview", icon: "business_chart-pie-36", component: Dashboard },
-    { path: "/icons", name: "Icons", icon: "design_image", component: Icons },
-    { path: "/maps", name: "Web3", icon: "business_money-coins", component: Maps },
-    { path: "/notifications", name: "Notifications", icon: "ui-1_bell-53", component: Notifications },
-    { path: "/user-page", name: "User Profile", icon: "users_single-02", component: UserPage },
-    { path: "/extended-tables", name: "Table List", icon: "files_paper", component: TableList },
-    { path: "/typography", name: "Typography", icon: "design-2_ruler-pencil", component: Typography },
+    // { path: "/icons", name: "Icons", icon: "design_image", component: Icons },
+    { path: "/web3", name: "Web3", icon: "business_money-coins", component: Maps },
+    // { path: "/notifications", name: "Notifications", icon: "ui-1_bell-53", component: Notifications },
+    { path: "/user-profile", name: "User Profile", icon: "users_single-02", component: UserPage },
+    // { path: "/extended-tables", name: "Table List", icon: "files_paper", component: TableList },
+    // { path: "/typography", name: "Typography", icon: "design-2_ruler-pencil", component: Typography },
     { redirect: true, path: "/", pathTo: "/dashboard", name: "Dashboard" }
 ];
 export default dashRoutes;
