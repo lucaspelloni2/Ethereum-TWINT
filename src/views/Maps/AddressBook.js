cb79af7d19a2cbd86acfe6639da476239e8896f2
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-05-22 18:59:36 +0200

    address book data and form

---
+++ src/views/Maps/AddressBook.js
@@ -0,0 +1,28 @@
+const AddressBook = [
+  {
+    name: 'Lucas Pelloni',
+    address: '0x199d7e3cE60B45De7ee29E0c3a1a2d66197A20D4'
+  },
+  {
+    name: 'Severin Wullschleger',
+    address: '0x199d7e3cE60B45De7ee29E0c3a1a2d66197A20D4'
+  },
+  {
+    name: 'Andreas Schaufehubul',
+    address: '0x199d7e3cE60B45De7ee29E0c3a1a2d66197A20D4'
+  },
+  {
+    name: 'Vitalik Buterin',
+    address: '0x199d7e3cE60B45De7ee29E0c3a1a2d66197A20D4'
+  },
+  {
+    name: 'Satoshi Nakamoto',
+    address: '0x199d7e3cE60B45De7ee29E0c3a1a2d66197A20D4'
+  },
+  {
+    name: 'Jonathan Burger',
+    address: '0x199d7e3cE60B45De7ee29E0c3a1a2d66197A20D4'
+  }
+];
+
+export default AddressBook;
--- src/views/Maps/Maps.jsx
+++ src/views/Maps/Maps.jsx
@@ -1,104 +1,135 @@
 import React from 'react';
 import {
-  Row, Col,
-  Card, CardHeader, CardBody,
-    Button, Input, Label
-
+  Row,
+  Col,
+  Card,
+  CardHeader,
+  CardBody,
+  Button,
+  Input,
+  Label
 } from 'reactstrap';
 
-
-import { PanelHeader } from 'components';
+import {PanelHeader} from 'components';
 import Web3 from 'web3';
+import AddressBook from './AddressBook';
 
+class FullScreenMap extends React.Component {
+  constructor() {
+    super();
 
+    let web3 = new Web3(Web3.givenProvider);
 
+    this.state = {
+      account: {
+        ethAddress: '',
+        ethBalance: 0
+      },
+      web3: web3,
+      addresses: [],
 
-class FullScreenMap extends React.Component{
-    constructor() {
-        super();
+      selectedAddress: 'default',
+      amount: '0.00'
+    };
+  }
 
-        let web3 = new Web3(Web3.givenProvider);
+  async componentDidMount() {
+    let addresses = await this.getUserAddresses();
+    let address = addresses[0];
+    let balance = await this.state.web3.eth.getBalance(address);
+    this.setState({
+      account: {
+        address,
+        balance: this.state.web3.utils.fromWei(balance, 'ether')
+      },
+      addresses: addresses
+    });
+    console.log(this.state);
+  }
 
-        this.state = {
-            account: {
-                ethAddress: '',
-                ethBalance: 0
-            },
-            web3: web3,
-            addresses: [],
+  getUserAddresses() {
+    return this.state.web3.eth
+      .getAccounts()
+      .then(addresses => {
+        return addresses;
+      })
+      .catch(err => {
+        console.log('error getting address ' + err);
+      });
+  }
 
-            selectedAddress:"default",
-            amount: '0.00'
-        };
-    }
+  transferMoney() {
+    this.state.web3.eth.sendTransaction({
+      from: this.state.account.address,
+      to: this.state.selectedAddress,
+      value: this.state.web3.utils.toWei(this.state.amount, 'ether')
+    });
+    console.log('this address ', this.state.selectedAddress);
+  }
 
-    async componentDidMount() {
-        let addresses = await this.getUserAddresses();
-        let address = addresses[0];
-        let balance = await this.state.web3.eth.getBalance(address);
-        this.setState({
-            account: {address, balance: this.state.web3.utils.fromWei(balance, 'ether')},
-            addresses: addresses
-        });
-        console.log(this.state);
-    }
+  handleAddressChange(e) {
+    this.setState({selectedAddress: e.target.value});
+  }
 
-    getUserAddresses() {
-        return this.state.web3.eth
-            .getAccounts()
-            .then(addresses => {
-                return addresses;
-            })
-            .catch(err => {
-                console.log('error getting address ' + err);
-            });
-    }
+  handleAmountChange(e) {
+    this.setState({amount: e.target.value});
+  }
 
-    transferMoney() {
-        this.state.web3.eth.sendTransaction({
-            from: this.state.account.address,
-            to: this.state.selectedAddress,
-            value: this.state.web3.utils.toWei(this.state.amount, 'ether')
-        });
-        console.log("this address ", this.state.selectedAddress)
-    }
+  render() {
+    return (
+      <div>
+        <PanelHeader size="sm" />
+        <div className="content">
+          <Row>
+            <Col xs={6}>
+              <Card>
+                <CardHeader>Send your money</CardHeader>
+                <CardBody>
+                  <Label for={'address'}>Address</Label>
+                  <Input
+                    id="address"
+                    placeholder={'Insert an address'}
+                    onChange={this.handleAddressChange.bind(this)}
+                  />
+                  <Label for={'amount'}>Amount (in ETH)</Label>
+                  <Input
+                    id="amount"
+                    placeholder={'Insert your amount'}
+                    onChange={this.handleAmountChange.bind(this)}
+                  />
+                  <Button
+                    onClick={() => {
+                      this.transferMoney();
+                    }}
+                  >
+                    Send money
+                  </Button>
+                </CardBody>
+              </Card>
+            </Col>
 
-    handleAddressChange(e) {
-        this.setState({selectedAddress: e.target.value});
-    }
-
-    handleAmountChange(e) {
-        this.setState({amount: e.target.value});
-    }
-
-        render(){
-        return (
-          <div>
-            <PanelHeader size="sm" />
-            <div className="content">
-              <Row>
-                <Col xs={12}>
-                  <Card>
-                    <CardHeader>Web 3 Integration</CardHeader>
-                    <CardBody>
-
-                        <p>TODO: Integrate WEB 3 JS here!</p>
-                        <Label for={"address"}>Address</Label>
-                        <Input id="address" placeholder={"Insert an address"} onChange={this.handleAddressChange.bind(this)}/>
-                        <Label for={"amount"}>Amount (in ETH)</Label>
-                        <Input id="amount" placeholder={"Insert your amount"} onChange={this.handleAmountChange.bind(this)}/>
-                        <Button onClick={()=>{
-                            this.transferMoney()
-                        }
-                        }>Send money</Button>
-                    </CardBody>
-                  </Card>
-                </Col>
-              </Row>
-            </div>
-          </div>
-        );
-    }
+            <Col xs={6}>
+              <Card>
+                <CardHeader>Search an address</CardHeader>
+                <CardBody>
+                  <Label>Select an address from your book</Label>
+                  <Input type="select" name="select" id="exampleSelect">
+                    {AddressBook.map(obj => {
+                      return (
+                        <option id={obj.address} value={obj}>
+                          {obj.name} ({obj.address})
+                        </option>
+                      );
+                    })}
+                  </Input>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default FullScreenMap;
