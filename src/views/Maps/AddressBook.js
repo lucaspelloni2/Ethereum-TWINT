9a3127d1fe6eeb710c7d6d09f3642115269b71e5
severinwullschleger <severin.wullschleger@bluewin.ch>
2018-05-23 10:08:28 +0200

    updated addresses

--- src/views/Maps/AddressBook.js
+++ src/views/Maps/AddressBook.js
@@ -1,11 +1,11 @@
 const AddressBook = [
   {
-    name: 'Lucas Pelloni',
+    name: 'Lukas Pelloni',
     address: '0x199d7e3cE60B45De7ee29E0c3a1a2d66197A20D4'
   },
   {
     name: 'Severin Wullschleger',
-    address: '0x199d7e3cE60B45De7ee29E0c3a1a2d66197A20D4'
+    address: '0x8745BE2c582BCFC50ACF9d2C61CadEd65a4E3825'
   },
   {
     name: 'Andreas Schaufehubul',
@@ -13,7 +13,7 @@ const AddressBook = [
   },
   {
     name: 'Vitalik Buterin',
-    address: '0x199d7e3cE60B45De7ee29E0c3a1a2d66197A20D4'
+    address: '0xc7470e71627eA66bBFBfD275A616b07273295493'
   },
   {
     name: 'Satoshi Nakamoto',
