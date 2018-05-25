e4cbc086a1209b23af10a93590bbf73d41d94bc2
lucaspelloni2 <lucas_pelloni@hotmail.com>
2018-05-25 10:30:25 +0200

    Creditor as you displayed

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -1,131 +1,140 @@
 import React from 'react';
 import {
-  Card, CardHeader, CardBody, CardFooter, CardTitle, Row, Col,
-  Table, ButtonGroup, Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  Row,
+  Col,
+  Table,
+  ButtonGroup,
+  Button
 } from 'reactstrap';
-import { Line } from 'react-chartjs-2';
-import {
-    PanelHeader, Stats, Tasks
-} from 'components';
+import {Line} from 'react-chartjs-2';
+import {PanelHeader, Stats, Tasks} from 'components';
 
-import {
-    dashboardPanelChart
-} from 'variables/charts.jsx';
+import {dashboardPanelChart} from 'variables/charts.jsx';
 
-import { tasks } from 'variables/general.jsx';
-import MonthChart from "../../components/MonthChart/MonthChart";
-import YearChart from "../../components/YearChart/YearChart";
-import DayChart from "../../components/DayChart/DayChart";
+import {tasks} from 'variables/general.jsx';
+import MonthChart from '../../components/MonthChart/MonthChart';
+import YearChart from '../../components/YearChart/YearChart';
+import DayChart from '../../components/DayChart/DayChart';
 
-class Dashboard extends React.Component{
-
-    constructor(props) {
-      super(props);
-      this.state = {
-        crypto: 'ETH'
-      };
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      crypto: 'ETH'
+    };
+  }
+  changeCrypto(crypto) {
+    let currency;
+    switch (crypto) {
+      case 'ETH':
+        currency = 'ETH';
+        break;
+      case 'BTC':
+        currency = 'BTC';
+        break;
+      case 'XRP':
+        currency = 'XRP';
+        break;
+      default:
+        currency = 'ETH';
     }
+    this.setState({
+      crypto: currency
+    });
+  }
 
-
-
-      changeCrypto(crypto) {
-      let currency;
-      switch (crypto) {
-        case 'ETH':
-          currency = 'ETH';
-          break;
-        case 'BTC':
-          currency = 'BTC';
-          break;
-        case 'XRP':
-          currency = 'XRP';
-          break;
-        default:
-          currency = 'ETH';
-      }
-      this.setState({
-        crypto: currency
-      });
-      }
-
-    render(){
-
-        return (
-            <div>
-                <PanelHeader
-                    size="lg"
-                    content={
-                        <Line data={dashboardPanelChart.data} options={dashboardPanelChart.options}/>
-                    }
-                />
-                <div className="content">
-                  <Row>
-                    <Col xs={12} md={6}>
-                    <ButtonGroup>
-                      <Button onClick={() => this.changeCrypto('ETH')}>Ethereum</Button>
-                      <Button onClick={() => this.changeCrypto('BTC')}>Bitcoin</Button>
-                      <Button onClick={() => this.changeCrypto('XRP')}>Ripple</Button>
-                    </ButtonGroup>
-                    </Col>
-                  </Row>
-                    <Row>
-                       <MonthChart crypto={this.state.crypto}/>
-                        <YearChart crypto={this.state.crypto}/>
-                        <DayChart crypto={this.state.crypto}/>
-                    </Row>
-                    <Row>
-                      <Col xs={12} md={6}>
-                        <Card className="card-tasks">
-                          <CardHeader>
-                            <CardTitle>Tasks</CardTitle>
-                            <p className="category">Crypto News</p>
-                          </CardHeader>
-                          <CardBody>
-                            <Tasks tasks={tasks}/>
-                          </CardBody>
-                          <CardFooter>
-                            <hr />
-                            <Stats>
-                                {[
-                                    { i: "now-ui-icons loader_refresh spin", t: "Updated 3 minutes ago"}
-                                ]}
-                            </Stats>
-                          </CardFooter>
-                        </Card>
-                      </Col>
-                      <Col xs={12} md={6}>
-                        <Card>
-                          <CardHeader>
-                            <CardTitle>Crypto Prices</CardTitle>
-                            <p className="category">From Coinmarketcap</p>
-                          </CardHeader>
-                          <CardBody>
-                            <Table responsive>
-                              <thead className=" text-primary">
-                                <tr>
-                                  <th>Currency</th>
-                                <th>Price</th>
-                                <th>Last 24h</th>
-                                <th className="text-right">Market Cap</th>
-                                </tr>
-                              </thead>
-                              <tbody>
-                                <tr>
-                                  <td>todo</td>
-                                  <td>todo</td>
-                                  <td>todo</td>
-                                  <td className="text-right">todo</td>
-                                </tr>
-                              </tbody>
-                            </Table>
-                          </CardBody>
-                        </Card>
-                      </Col>
-                    </Row>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <PanelHeader
+          size="lg"
+          content={
+            <Line
+              data={dashboardPanelChart.data}
+              options={dashboardPanelChart.options}
+            />
+          }
+        />
+        <div className="content">
+          <Row>
+            <Col xs={12} md={6}>
+              <ButtonGroup>
+                <Button onClick={() => this.changeCrypto('ETH')}>
+                  Ethereum
+                </Button>
+                <Button onClick={() => this.changeCrypto('BTC')}>
+                  Bitcoin
+                </Button>
+                <Button onClick={() => this.changeCrypto('XRP')}>Ripple</Button>
+              </ButtonGroup>
+            </Col>
+          </Row>
+          <Row>
+            <MonthChart crypto={this.state.crypto} />
+            <YearChart crypto={this.state.crypto} />
+            <DayChart crypto={this.state.crypto} />
+          </Row>
+          <Row>
+            <Col xs={12} md={6}>
+              <Card className="card-tasks">
+                <CardHeader>
+                  <CardTitle>Tasks</CardTitle>
+                  <p className="category">Crypto News</p>
+                </CardHeader>
+                <CardBody>
+                  <Tasks tasks={tasks} />
+                </CardBody>
+                <CardFooter>
+                  <hr />
+                  <Stats>
+                    {[
+                      {
+                        i: 'now-ui-icons loader_refresh spin',
+                        t: 'Updated 3 minutes ago'
+                      }
+                    ]}
+                  </Stats>
+                </CardFooter>
+              </Card>
+            </Col>
+            <Col xs={12} md={6}>
+              <Card>
+                <CardHeader>
+                  <CardTitle>Crypto Prices</CardTitle>
+                  <p className="category">From Coinmarketcap</p>
+                </CardHeader>
+                <CardBody>
+                  <Table responsive>
+                    <thead className=" text-primary">
+                      <tr>
+                        <th>Currency</th>
+                        <th>Price</th>
+                        <th>Last 24h</th>
+                        <th className="text-right">Market Cap</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>todo</td>
+                        <td>todo</td>
+                        <td>todo</td>
+                        <td className="text-right">todo</td>
+                      </tr>
+                    </tbody>
+                  </Table>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Dashboard;
--- src/views/Maps/OpenRequests.js
+++ src/views/Maps/OpenRequests.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import {Button, Card, CardBody, CardHeader, Table} from "reactstrap";
-import AddressBook from "./AddressBook";
-import '../../assets/css/demo.css'
-import {ClipLoader} from "react-spinners";
-
+import {Button, Card, CardBody, CardHeader, Table} from 'reactstrap';
+import AddressBook from './AddressBook';
+import '../../assets/css/demo.css';
+import {ClipLoader} from 'react-spinners';
 
 class OpenRequests extends React.Component {
   constructor(props) {
@@ -11,14 +10,14 @@ class OpenRequests extends React.Component {
     this.state = {
       requests: [],
       pendingRequests: new Set()
-    }
+    };
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.requests !== this.state.requests) {
       this.setState({
         requests: nextProps.requests
-      })
+      });
     }
   }
 
@@ -26,16 +25,31 @@ class OpenRequests extends React.Component {
     let addrShort = address.substring(0, 10);
     const accounts = AddressBook.getAccounts();
 
-    let knownAccount = accounts.find(a => a.address.toLowerCase() === address.toLowerCase());
+    let knownAccount = accounts.find(
+      a => a.address.toLowerCase() === address.toLowerCase()
+    );
 
     if (knownAccount) {
       return (
         <div>
           <a
-            href={'https://ropsten.etherscan.io/address/' + knownAccount.address}
+            href={
+              'https://ropsten.etherscan.io/address/' + knownAccount.address
+            }
             target="_blank"
           >
             {knownAccount.name}
+          </a>
+        </div>
+      );
+    } else if (address === this.props.account.ethAddress) {
+      return (
+        <div>
+          <a
+            href={'https://ropsten.etherscan.io/address/' + address}
+            target="_blank"
+          >
+            You
           </a>
         </div>
       );
@@ -43,7 +57,10 @@ class OpenRequests extends React.Component {
       return (
         <div>
           {' '}
-          <a href={'https://ropsten.etherscan.io/address/' + address} target="_blank">
+          <a
+            href={'https://ropsten.etherscan.io/address/' + address}
+            target="_blank"
+          >
             {addrShort}..
           </a>
         </div>
@@ -57,17 +74,15 @@ class OpenRequests extends React.Component {
   fulfillRequest(reqId, valueInEth) {
     let pending = this.state.pendingRequests;
     this.setState({
-        pendingRequests: pending.add(reqId)
-      }
-    );
-    this.props.contract.methods.fulfillRequest(reqId)
+      pendingRequests: pending.add(reqId)
+    });
+    this.props.contract.methods
+      .fulfillRequest(reqId)
       .send({
         from: this.props.account.ethAddress,
         value: this.props.web3.utils.toWei(valueInEth, 'ether')
       })
-      .on('transactionHash', tx => {
-
-      })
+      .on('transactionHash', tx => {})
       .on('receipt', res => {
         if (res.status) {
           pending = this.state.pendingRequests;
@@ -79,9 +94,7 @@ class OpenRequests extends React.Component {
           console.log('fail');
         }
       })
-      .on('confirmation', function (confirmationNr) {
-
-      });
+      .on('confirmation', function(confirmationNr) {});
   }
 
   /*
@@ -92,13 +105,12 @@ class OpenRequests extends React.Component {
     this.setState({
       pendingRequests: pending.add(reqId)
     });
-    this.props.contract.methods.withdrawRequest(reqId)
+    this.props.contract.methods
+      .withdrawRequest(reqId)
       .send({
         from: this.props.account.ethAddress
       })
-      .on('transactionHash', tx => {
-
-      })
+      .on('transactionHash', tx => {})
       .on('receipt', res => {
         if (res.status) {
           pending = this.state.pendingRequests;
@@ -110,11 +122,8 @@ class OpenRequests extends React.Component {
           console.log('fail');
         }
       })
-      .on('confirmation', function (confirmationNr) {
-
-      });
+      .on('confirmation', function(confirmationNr) {});
   }
-
 
   renderAction(request) {
     let pending = this.state.pendingRequests;
@@ -123,80 +132,98 @@ class OpenRequests extends React.Component {
       if (pending.has(parseInt(request.reqId))) {
         return (
           <td>
-            <ClipLoader
-              size={35}
-              color={'#00aaff'}/>
+            <ClipLoader size={35} color={'#00aaff'} />
           </td>
-        )
+        );
       }
     }
     if (request.state === '1') {
       if (this.props.account.ethAddress === request.creditor) {
         return (
-          <td><Button style={{background: '#00aaff'}} onClick={() => (this.withdrawRequest(parseInt(request.reqId)))}>Withdraw
-            Request</Button></td>
-        )
+          <td>
+            <Button
+              style={{background: '#00aaff'}}
+              onClick={() => this.withdrawRequest(parseInt(request.reqId))}
+            >
+              Withdraw Request
+            </Button>
+          </td>
+        );
       } else {
         return (
-          <td><Button color={'primary'} onClick={() => (this.fulfillRequest(parseInt(request.reqId), request.value))}>Fullfill
-            Request</Button></td>
-        )
+          <td>
+            <Button
+              color={'primary'}
+              onClick={() =>
+                this.fulfillRequest(parseInt(request.reqId), request.value)
+              }
+            >
+              Fullfill Request
+            </Button>
+          </td>
+        );
       }
     } else if (request.state === '2') {
       return (
         <td>
-          <div><i className="now-ui-icons business_bank"/> Withdrawn</div>
+          <div>
+            <i className="now-ui-icons business_bank" /> Withdrawn
+          </div>
         </td>
       );
     } else {
       return (
-        <td><i className="now-ui-icons ui-1_check"
-               style={{color: '#092', fontSize: 20, fontWeight: 900}}/>
+        <td>
+          <i
+            className="now-ui-icons ui-1_check"
+            style={{color: '#092', fontSize: 20, fontWeight: 900}}
+          />
           Paid
         </td>
       );
     }
   }
 
-
   render() {
     return (
       <Card>
         <CardHeader> My Open Requests</CardHeader>
-        <CardBody>{this.state.requests.length <= 0 ? (
-          <div>You have no open requests</div>
-        ) : (
-          <div style={{overflow: 'scroll', maxHeight: 220}}>
-            <Table responsive>
-              <thead className="request-table-head">
-              <tr>
-                <th>RequestId</th>
-                <th>Creditor</th>
-                <th>Debitor</th>
-                <th>Value</th>
-                <th>Reason</th>
-                <th>Action</th>
-              </tr>
-              </thead>
-              <tbody>
-              {this.state.requests.map((request, i) => {
-                return (
-                  <tr key={i}>
-                    {<td>{request.reqId}</td>}
-                    {<td>{this.renderNameOrTxs(request.creditor)}</td>}
-                    {<td>{this.renderNameOrTxs(request.debitor)}</td>}
-                    {<td> {`${request.value} ETH`}</td>}
-                    {<td>{request.reason}</td>}
-                    {this.renderAction(request)}
+        <CardBody>
+          {this.state.requests.length <= 0 ? (
+            <div>You have no open requests</div>
+          ) : (
+            <div style={{overflow: 'scroll', maxHeight: 220}}>
+              <Table responsive>
+                <thead className="request-table-head">
+                  <tr>
+                    <th>RequestId</th>
+                    <th>Creditor</th>
+                    <th>Debitor</th>
+                    <th>Value</th>
+                    <th>Reason</th>
+                    <th>Action</th>
                   </tr>
-                )
-              })}
-              </tbody>
-            </Table>
-          </div>
-        )}</CardBody>
+                </thead>
+                <tbody>
+                  {this.state.requests.map((request, i) => {
+                    return (
+                      <tr key={i}>
+                        {<td>{request.reqId}</td>}
+                        {<td>{this.renderNameOrTxs(request.creditor)}</td>}
+                        {<td>{this.renderNameOrTxs(request.debitor)}</td>}
+                        {<td> {`${request.value} ETH`}</td>}
+                        {<td>{request.reason}</td>}
+                        {this.renderAction(request)}
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </Table>
+            </div>
+          )}
+        </CardBody>
       </Card>
-    )
+    );
   }
 }
 
